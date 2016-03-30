ed634d915af7be9a981bf26f5658d772f08b648b
Jesse McClure <jesse.mcclure@umassmed.edu>
2016-03-30 14:15:45 -0400

    full rewrite to "unkludge" percontation

--- percontation.c
+++ percontation.c
@@ -2,128 +2,93 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-#define MAX_LINE	256
-
-#define OPT_NORM	0x00
-#define OPT_EXEC	0x01
-#define OPT_DIR	0x02
-#define OPT_PATH	0x04
-#define OPT_DESK	0x10
-#define OPT_DCOM	0x20
-
-static const char *pre, *str;
+static const int MAX_LINE = 256;
 static const char deskchar = '>';
 
-
-static int get_name(const char *path,int opt) {
+static int get_name(const char *pre, const char *path, const char *match, int mode) {
 	DIR *dir;
-	FILE *f;
 	struct dirent *de;
 	struct stat info;
-	int opts, match, term=0;
-	char line[MAX_LINE], *name=NULL, *c, *c2, *exec=NULL;
-	if (path[0] == '\0') {
-		dir = opendir("/");
-		chdir("/");
-	}
-	else {
-		dir = opendir(path);
-		chdir(path);
-	}
-	if (!dir) return 1;
+	const char *p = (path[0] == '\0' ? "/" : path);
+	if (!(dir=opendir(p))) return 1;
+	chdir(p);
 	while ( (de=readdir(dir)) ) {
-		if (de->d_name[0] == '.') continue;
-		else if (opt & OPT_DESK) {
-			name = strdup(de->d_name);
-			if ( (c=strrchr(name,'.')) ) *c='\0';
-			if (!(opt & OPT_DCOM)) { /* desktop */
-				if (!strncmp(de->d_name,str,strlen(str)))
-					printf("%c%s\n",deskchar,name);
-			}
-			else { /* desktop+ */
-				f = fopen(de->d_name,"r");
-				match = 0;
-				while(fgets(line,MAX_LINE,f)) {
-					if (	(strncmp(line,"Name",4)==0 ||
-							strncmp(line,"GenericName",11)==0) &&
-							(c=strchr(line,'=')) && c++ &&
-							strncasecmp(str,c,strlen(str))==0 )
-						match = 1;
-					if (	strncmp(line,"Comment",7)==0 &&
-							(c=strchr(line,'=')) && c++ &&
-							strcasestr(c,str) )
-						match=1;
-				}
-				if (name && match) printf("%c%s\n",deskchar,name);
-				fclose(f);
-			}
-			if (name) { free(name); name=NULL; }
-		}
-		else {
-			if (strncmp(de->d_name,str,strlen(str))) continue;
-			stat(de->d_name,&info);
-			if( (opt & OPT_EXEC) && info.st_mode & 0111) opts = opt;
-			else if (S_ISDIR(info.st_mode)) opts = opt | OPT_DIR;
-			else opts = opt;
-			printf(pre);
-			if (opts & OPT_PATH) printf("%s/",path);
-			if (opts & OPT_DIR) printf("%s/%s/\n",path,de->d_name);
-			else printf("%s\n",de->d_name);
-		}
+		if (de->d_name[0] == '.' || strncmp(de->d_name, match, strlen(match))) continue;
+		stat(de->d_name, &info);
+		if(!(info.st_mode & mode)) continue;
+		printf(pre);
+		if (mode & 0100000 || pre[0] != '\0') printf("%s/",path);
+		if (S_ISDIR(info.st_mode)) printf("%s/\n", de->d_name);
+		else printf("%s\n", de->d_name);
 	}
 	closedir(dir);
 	return 0;
 }
 
-static int from_path() {
-	char *path;
-	const char *pwd = getenv("PWD");
-	if ( !(path=getenv("PATH")) ) return 2;
-	char *list = strdup(path), *next = list, *delim;
-	while ( (delim=strchr(next,':')) ) {
-		*delim = '\0';
-		get_name(next,OPT_EXEC);
-		next = delim+1;
-	}
+static int from_path(const char *pre, const char *str) {
+	char *path, *next;
+	const char *name;
+	int mode = 0;
 	if (str[0] == '/') {
 		path = strdup(str);
-		char *c = strrchr(path,'/');
-		*c = '\0';
-		str = strrchr(str,'/');
-		str++;
-		if (path[0] == '\0') get_name("",OPT_PATH);
-		else get_name(path,OPT_PATH);
-		free(path);
+		*strrchr(path,'/') = '\0';
+		name = strrchr(str,'/') + 1;
+		mode = 01000000;
 	}
 	else {
-		get_name(pwd,OPT_NORM);
+		path = strdup(getenv("PWD"));
+		name = str;
 	}
-	chdir(pwd);
+	if (!pre || pre[0] == '\0') {
+		if ( !(path=strdup(getenv("PATH"))) ) return 1;
+		for (next = strtok(path,":"); next; next = strtok(NULL,":")) get_name(pre, next, name, mode | 0111);
+	}
+	else {
+		get_name(pre, path, name, mode | 0444);
+	}
+	free(path);
 	return 0;
 }
 
-static int from_desktops(int comments) {
-	// only run this if pre is empty
-	// for all paths to .desktop files
-	get_name("/usr/share/applications",
-			OPT_DESK | (comments ? OPT_DCOM : 0));
+static int from_desktop(int in, const char *str) {
+	DIR *dir;
+	FILE *fp;
+	char *name, *c, line[MAX_LINE];
+	if ( !(dir=opendir("/usr/share/applications")) ) return 1;
+	chdir("/usr/share/applications");
+	struct dirent *de;
+	while ( (de=readdir(dir)) ) {
+		if (de->d_name[0] == '.') continue;
+		name = strdup(de->d_name);
+		if ( (c=strrchr(name,'.')) ) *c = '\0';
+		if (!strncmp(de->d_name, str, strlen(str))) printf("%c%s\n",deskchar,name);
+		else if (in) {
+			if (!(fp=fopen(de->d_name, "r"))) continue;
+			while (fgets(line,MAX_LINE,fp)) {
+				if (strncmp(line,"Name=",5) && strncmp(line,"GenericName=",12)) continue;
+				if (!(c=strchr(line,'='))) continue;
+				if (!strncasecmp(++c,str,strlen(str))) printf("%c%s\n",deskchar,name);
+			}
+			fclose(fp);
+		}
+		free(name);
+	}
 	return 0;
 }
 
 int main(int argc, const char **argv) {
 	int i;
 	if (argc < 3) return 1;
-	pre = argv[argc - 2];
-	str = argv[argc - 1];
 	for (i = 1; i < argc - 2; i++) {
-		if (strncmp(argv[i],"path",4)==0) from_path();
-		else if (strncmp(argv[i],"desktop+",9)==0) from_desktops(1);
-		else if (strncmp(argv[i],"desktop",8)==0) from_desktops(0);
+		if (!strncmp(argv[i],"path",4)) from_path(argc[argv-2],argc[argv-1]);
+		else if (!strncmp(argv[i],"desktop+",9)) from_desktop(1,argc[argv-1]);
+		else if (!strncmp(argv[i],"desktop",8)) from_desktop(0,argc[argv-1]);
 	}
-	if (i == 1) from_path();
+	if (i == 1) from_path(argc[argv-2],argc[argv-1]);
 	return 0;
 }
 
